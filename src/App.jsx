d4d0e904d0e7a860bc5b9d0d39f03a8a619b2760
poodlepoodle <chammal97@naver.com>
2023-11-15 01:27:48 +0900

    refactor: App.js 기본 코드 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import Layout from "./components/Layout";
-import { useMediaQuery } from "react-responsive";
+import './App.css';
+
+import Layout from './components/Layout';
 
 function App() {
   return (
