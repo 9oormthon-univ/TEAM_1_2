38c721aaee8ee358bbfead01692497d2e69d616a
sem-git <lsmsm0327@naver.com>
2023-11-17 21:02:28 +0900

    feat: 알림, 검색, 마이페이지 구현

--- src/pages/mypage/MyPage.jsx
+++ src/pages/mypage/MyPage.jsx
@@ -1,5 +1,161 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const MyProfile = styled.div`
+  position: relative;
+  display: flex;
+  margin-top: 3rem;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+
+  width: 24.375rem;
+  height: 9.125rem;
+  background: #fff;
+  box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const PersonalData = styled.div`
+  position: relative;
+  text-align: left;
+  margin: 1rem;
+
+  font-family: AppleSDGothicNeoEB00;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #333;
+`;
+
+const Pic = styled.div`
+  position: relative;
+  margin: 1rem;
+
+  width: 6rem;
+  height: 6rem;
+  background: #d9d9d9;
+  border-radius: 5rem;
+`;
+
+const MenuBox = styled.div`
+  position: relative;
+  display: flex;
+  margin: 3rem auto;
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+
+  width: 22.375rem;
+  height: 6.75rem;
+  border-radius: 1.6875rem;
+  box-shadow: 0px 2px 6px 0px rgba(0, 0, 0, 0.1);
+`;
+
+const Menu = styled.div`
+  position: relative;
+  display: flex;
+  margin: auto;
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+
+  font-family: AppleSDGothicNeoSB00;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #333;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const Hr = styled.div`
+  position: relative;
+
+  width: 19.875rem;
+  height: 0.05rem;
+  background: #f0f0f0;
+`;
+
 const MyPage = () => {
-  return <></>;
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost:5173/user/profile');
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Error fetching user profile:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return (
+    <>
+      <MyProfile>
+        <PersonalData>
+          <span style={{ fontSize: '1.25rem' }}>{userData.nickname}</span>
+          <br />
+          <span
+            style={{ fontSize: '0.875rem' }}
+          >{`@${userData.account_id}`}</span>
+        </PersonalData>
+        <Pic style={{ backgroundImage: `url(${userData.image})` }} />
+      </MyProfile>
+      <MenuBox>
+        <Menu>
+          <div
+            style={{
+              marginRight: '13rem',
+            }}
+          >
+            공지사항
+          </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M12.9462 11.9995L8.34619 7.39953L9.40002 6.3457L15.0538 11.9995L9.40002 17.6534L8.34619 16.5995L12.9462 11.9995Z"
+              fill="#BFBFBF"
+            />
+          </svg>
+        </Menu>
+        <Hr></Hr>
+        <Menu>
+          <div
+            style={{
+              marginRight: '13rem',
+            }}
+          >
+            정보수정
+          </div>
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M12.9462 11.9995L8.34619 7.39953L9.40002 6.3457L15.0538 11.9995L9.40002 17.6534L8.34619 16.5995L12.9462 11.9995Z"
+              fill="#BFBFBF"
+            />
+          </svg>
+        </Menu>
+      </MenuBox>
+    </>
+  );
 };
 
 export default MyPage;
--- src/pages/search/SearchPage.jsx
+++ src/pages/search/SearchPage.jsx
@@ -1,5 +1,190 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const Top = styled.div`
+  position: relative;
+  display: flex;
+  margin-top: 2rem;
+`;
+
+const Back = styled.div`
+  position: relative;
+  margin-top: 0.5rem;
+  margin-left: 1.5rem;
+
+  :hover {
+    cursor: pointer;
+  }
+`;
+
+const SearchField = styled.input`
+  position: relative;
+  margin-left: 0.7rem;
+
+  width: 19.375rem;
+  height: 2.375rem;
+  border-radius: 0.625rem;
+  background: #f7f7f7;
+  border: none;
+  padding: 0.5rem;
+  box-sizing: border-box;
+
+  font-family: AppleSDGothicNeoR00;
+  font-size: 0.875rem;
+  font-style: normal;
+  font-weight: 400;
+
+  &::placeholder {
+    color: #8e8c86;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Hr = styled.div`
+  position: relative;
+  margin: 1rem auto;
+
+  width: 23.125rem;
+  height: 0.03125rem;
+  background: #d7d7d7;
+`;
+
+const Border = styled.div`
+  position: relative;
+  margin: 1rem auto;
+  overflow-y: auto;
+
+  width: 22.625rem;
+  height: 38rem;
+  border: 1px solid #000;
+`;
+
+const ProfileBorder = styled.div`
+  position: relative;
+  display: flex;
+  margin: 1rem auto;
+  flex-direction: row;
+  align-items: center;
+
+  width: 21.75rem;
+  height: 3.75rem;
+  border: 1px solid #000;
+`;
+
+const Pic = styled.div`
+  position: relative;
+  margin-left: 0.5rem;
+
+  width: 3rem;
+  height: 3rem;
+  background: #d9d9d9;
+  border-radius: 3rem;
+`;
+
+const PersonalData = styled.div`
+  position: relative;
+  margin: 1rem;
+  text-align: left;
+
+  font-family: AppleSDGothicNeoEB00;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  color: #333;
+`;
+
 const SearchPage = () => {
-  return <></>;
+  const [searchResult, setSearchResult] = useState([]);
+  const [searchID, setSearchID] = useState('');
+
+  const handleChange = (event) => {
+    setSearchID(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:5173/friend/search?keyword=${searchID}`
+      );
+      if (response.status === 200) {
+        setSearchResult(response.data);
+      } else if (response.status === 404) {
+        setSearchResult([]);
+      } else {
+        console.error('Unexpected response:', response);
+      }
+    } catch (error) {
+      console.error('Error fetching search results:', error);
+    }
+  };
+
+  const renderFriendshipStatus = (status) => {
+    switch (status) {
+      case 'FRIEND':
+        return '이미 친구입니다';
+      case 'SENT':
+        return '요청 보낸 상태입니다';
+      case 'RECEIVED':
+        return '요청 받은 상태입니다';
+      case 'UNFRIEND':
+        return '친구가 아닙니다';
+      case 'SELF':
+        return '본인입니다';
+      default:
+        return '알 수 없음';
+    }
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [searchID]);
+
+  return (
+    <>
+      <Top>
+        <Back>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+          >
+            <path
+              d="M9.17308 18.6635L2.5 11.9904L9.17308 5.31738L10.2173 6.36158L5.35377 11.2405H21.5096V12.7404H5.3634L10.2423 17.6193L9.17308 18.6635Z"
+              fill="#333333"
+            />
+          </svg>
+        </Back>
+        <SearchField
+          placeholder={'아이디로 친구를 검색하세요.'}
+          value={searchID}
+          onChange={handleChange}
+        />
+      </Top>
+      <Hr />
+      <Border>
+        {searchResult.map((result) => (
+          <ProfileBorder>
+            <Pic style={{ backgroundImage: `url(${result.image})` }} />
+            <PersonalData>
+              <span style={{ fontSize: '0.875rem', color: '#333' }}>
+                {`@${result.nickname}`}
+              </span>
+              <br />
+              <span style={{ fontSize: '0.75rem', color: '#C3C3C3' }}>
+                {`@${result.account_id}`} -{' '}
+                {renderFriendshipStatus(result.friendship_status)}
+              </span>
+            </PersonalData>
+          </ProfileBorder>
+        ))}
+      </Border>
+    </>
+  );
 };
 
 export default SearchPage;
