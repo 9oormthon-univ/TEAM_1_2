1f44b50ca541b5a46e94ad4c68858434fcae9472
sem-git <lsmsm0327@naver.com>
2023-11-17 22:53:55 +0900

    chore: 파일명 변경, mypage index 삭제

---
+++ src/pages/home/HomePage.jsx
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+
+const HomePage = () => {
+  return (
+    <>
+      <h1>Home page</h1>
+    </>
+  );
+};
+
+export default HomePage;
--- src/pages/mypage/index.jsx
+++
@@ -1,9 +0,0 @@
-const MyPage = () => {
-  return (
-    <>
-      <h1>My page</h1>
-    </>
-  );
-};
-
-export default MyPage;
